07e244f847ed66e62f066592040e518c447073e4
Hamza Sufian <hsufiian@yahoo.com>
2024-03-18 14:57:51 +0000

    vue

---
+++ TechThreadsGhana/Frontend/techthread/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/e-commerce-3692440_1280.jpg">
+  <HelloWorld msg="Welcome to TechThread App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-block-start: 60px;
+  margin-block-end: 60px;
+}
+</style>
